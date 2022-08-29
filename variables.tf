2d054065e905900a5b5ab7e2c0db67d157cf8c68
Johnny <91308486+panasyg@users.noreply.github.com>
2022-08-30 01:59:02 +0300

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "azure_subscription_description"{
-  type = map
-    default = {
-        "subscription_id" = "a492632e-e41e-409c-b017-962d2f6ba3e0"
-        "client_secret" = "8bG8Q~xAk4T3OW3OPMsFxGo16e_J6o81INSyra70"
-        "client_id" ="f3236fc7-d6fd-4553-a3b6-3e033b64d15a"
-        "tenant_id" = "1a9e7b07-fdc8-478f-ae12-90cf6134ec82"
-      }
-  }
